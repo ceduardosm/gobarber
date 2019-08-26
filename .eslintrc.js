d3e06f327bd71b6361c06cb3341de6069f46b8af
ceduardosm <carloseduardosm@outlook.com>
2019-08-26 03:33:42 -0300

    Autenticação JWT

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true,
   },
   extends: [ 'airbnb-base','prettier' ],
-  plugins: [ 'prettier'],
+  plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -15,9 +15,9 @@ module.exports = {
   },
   rules: {
     "prettier/prettier": "error",
-    "class-methods-user-this": "off",
+    "class-methods-use-this": "off",
     "no-param-reassign": "off",
     "camelcase": "off",
-    "no-unused-vars": ["error", { "agsIgnorePattern": "next" }]
+    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
   },
 };
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '5f88af81d7fdeca911e8a5e3c3118e14',
+  expiresIn: '1d',
+};
